c8c3e28504845c7c9fd26a031689068691153253
mei932 <meiyi1010302@gmail.com>
2023-11-30 23:36:25 +0700

    upload

---
+++ Btl_QuanLyNhaSach/Modify/Connection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Btl_QuanLyNhaSach
+{
+    class Connection
+    {
+        // Đường liên kết tới sql
+        private static string stringConnection = @"Data Source=DESKTOP-ESFTV9I\SQLEXPRESS;Initial Catalog=SQLHT1;Integrated Security=True";
+       
+
+        public static SqlConnection GetSqlConnection()
+        {
+            return new SqlConnection(stringConnection);
+        }
+    }
+}
+//Some files need fixing
+// connection
+// thongketheonam.cs
+//trangchu.cs
